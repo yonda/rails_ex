703929ae420836e196b03671df3d6494db3d0680
yonda <honda@33i.co.jp>
2013-06-24 00:28:39 +0900

    complete task D

--- depot/app/controllers/application_controller.rb
+++ depot/app/controllers/application_controller.rb
@@ -8,4 +8,13 @@ class ApplicationController < ActionController::Base
   def set_date
   	@date_loaded = Time.now
   end
+
+  private
+  	def current_cart
+  		Cart.find(session[:cart_id])
+  	rescue ActiveRecord::RecordNotFound
+  		cart = Cart.create
+  		session[:cart_id] = cart.id
+  		cart
+  	end
 end
