29cd83e45edd77ea6cc33eead8cf6ae4195af244
yonda <honda@33i.co.jp>
2013-06-23 23:19:59 +0900

    complete task C

--- depot/app/controllers/application_controller.rb
+++ depot/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :date_loaded
+  before_filter :set_date
   protect_from_forgery
+  def date_loaded
+  	return Time.now
+  end
+  def set_date
+  	@date_loaded = Time.now
+  end
 end
