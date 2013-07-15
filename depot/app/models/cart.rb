975c6c3d52eef49a67f73d3cc5993b996dfc3d70
yonda <honda@33i.co.jp>
2013-07-15 21:43:27 +0900

    品目に数量を保持できるようにした

--- depot/app/models/cart.rb
+++ depot/app/models/cart.rb
@@ -1,4 +1,13 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, dependent: :destroy
-  attr_accessible :cart_id, :product
+	attr_accessible :cart_id, :product
+	def add_product(product_id)
+		current_item = line_items.find_by_product_id(product_id)
+		if current_item
+			current_item.quantity += 1
+		else
+			current_item = line_items.build(product_id: product_id)
+		end
+		current_item
+	end
 end
