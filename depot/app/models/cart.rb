14285aba62b3b35c9a50689cd9c10ab48b2743f6
yonda <honda@33i.co.jp>
2013-07-15 22:57:25 +0900

    品目を商品ごとにサマれるようにした

--- depot/app/models/cart.rb
+++ depot/app/models/cart.rb
@@ -1,6 +1,6 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, dependent: :destroy
-	attr_accessible :cart_id, :product
+	attr_accessible :cart_id, :product, :line_items, :product_id, :quantity
 	def add_product(product_id)
 		current_item = line_items.find_by_product_id(product_id)
 		if current_item
